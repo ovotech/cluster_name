bbf7c58833fee4f992c4a8824d3ccbd98c258bf5
eversC <chris.every@ovoenergy.com>
2019-11-24 03:23:12 +0000

    Add logging

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
+// Copyright 2019 OVO Technology
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -6,20 +20,27 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/ovotech/cluster_name/pkg/log"
 )
 
 var (
 	clusterName string
 	user        string
 	pass        string
+	logger      = log.StdoutLogger().Sugar()
 )
 
 const (
-	errorString            = "Unauthorized."
-	pattern                = "/name"
-	port                   = "8090"
-	clusterNameUserVarName = "CLUSTER_NAME_U"
-	clusterNamePassVarName = "CLUSTER_NAME_P"
+	errorString    = "Unauthorized."
+	defaultPattern = "/name"
+	defaultPort    = "8090"
+	envVarPrefix   = "CLUSTER_NAME"
+	userVarName    = "USER"
+	passVarName    = "PASS"
+	patternVarName = "PATT"
+	portVarName    = "PORT"
+	metaURL        = "http://metadata/computeMetadata/v1/instance/attributes/cluster-name"
 )
 
 func name(w http.ResponseWriter, req *http.Request) {
@@ -28,43 +49,62 @@ func name(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	if !envVarsValid() {
-		fmt.Println("vars invalid")
-		os.Exit(1)
+		logger.Fatal("Invalid env vars")
 	}
-	user = os.Getenv(clusterNameUserVarName)
-	pass = os.Getenv(clusterNamePassVarName)
-	clusterName = getClusterName()
-	if clusterName == "" {
-		os.Exit(1)
+	user = os.Getenv(envVarName(userVarName))
+	pass = os.Getenv(envVarName(passVarName))
+	var err error
+	if clusterName, err = getClusterName(); err != nil {
+		logger.Fatal(err)
 	}
+	pattern := getEnv(envVarName(patternVarName), defaultPattern)
 	http.HandleFunc(pattern, auth(name))
+	port := getEnv(envVarName(patternVarName), defaultPort)
 	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
 
-func getClusterName() string {
-	url := "http://metadata/computeMetadata/v1/instance/attributes/cluster-name"
+func getClusterName() (name string, err error) {
+	url := metaURL
 	client := &http.Client{Timeout: time.Second * 10}
-	req, _ := http.NewRequest("GET", url, nil)
+	var req *http.Request
+	if req, err = http.NewRequest("GET", url, nil); err != nil {
+		return
+	}
 	req.Header.Set("Metadata-Flavor", "Google")
 	var resp *http.Response
-	var err error
 	if resp, err = client.Do(req); err != nil {
-
+		return
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	return string(body)
+	var body []byte
+	if body, err = ioutil.ReadAll(resp.Body); err != nil {
+		return
+	}
+	name = string(body)
+	return
 }
 
 func envVarsValid() (valid bool) {
-	return len(os.Getenv(clusterNameUserVarName)) > 0 &&
-		len(os.Getenv(clusterNamePassVarName)) > 0
+	return len(os.Getenv(envVarName(userVarName))) > 0 &&
+		len(os.Getenv(envVarName(passVarName))) > 0
+}
+
+func envVarName(suffix string) string {
+	return fmt.Sprintf("%s_%s", envVarPrefix, suffix)
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
 }
 
 func auth(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		basicUser, basicPass, _ := r.BasicAuth()
 		if !check(basicUser, basicPass) {
+			logger.Warnf("Failed auth attempt from %s", r.RemoteAddr)
 			http.Error(w, errorString, http.StatusUnauthorized)
 			return
 		}
